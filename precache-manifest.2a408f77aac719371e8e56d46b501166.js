40724d3457261cf9cd72af8facb7e3244c5aeaf2
fournotfourerror <fournotfourerror@gmail.com>
2020-04-08 17:03:15 +0530

    Updates

---
+++ precache-manifest.2a408f77aac719371e8e56d46b501166.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db18a3d5c9363cc9535e38a1bd69aea",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "45064102d5a09115bce7",
+    "url": "/profile/static/css/main.496605ca.chunk.css"
+  },
+  {
+    "revision": "9016b65fc28c1697b839",
+    "url": "/profile/static/js/2.5f9501c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/profile/static/js/2.5f9501c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45064102d5a09115bce7",
+    "url": "/profile/static/js/main.c13146eb.chunk.js"
+  },
+  {
+    "revision": "9cc5a1952a14b5bf2e6e",
+    "url": "/profile/static/js/runtime-main.04a5f981.js"
+  },
+  {
+    "revision": "b5fcf8177543a0677eecdac1ec8cab41",
+    "url": "/profile/static/media/facebook.b5fcf817.svg"
+  },
+  {
+    "revision": "8f8edc5fb64b9bd436834dc926ac992c",
+    "url": "/profile/static/media/github.8f8edc5f.svg"
+  },
+  {
+    "revision": "c0c32ea60815d3d72d2dbcb59b793ec3",
+    "url": "/profile/static/media/linkedin.c0c32ea6.svg"
+  },
+  {
+    "revision": "adf60a0b47a60a641ffcec012f7b9058",
+    "url": "/profile/static/media/profile.adf60a0b.jpeg"
+  },
+  {
+    "revision": "dd90206d4d813b02e3f92ed7373ae817",
+    "url": "/profile/static/media/twitter.dd90206d.svg"
+  }
+]);
